12a64d7bb0f24861ea94daf61cad5d3a0db5324e
github username <github email address>
2020-05-05 11:56:37 -0400

    added relationships between models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+    belongs_to :user
+    belongs_to :location 
+    has_many :plant_orders 
+    has_many :plants, through: :plant_orders 
+    has_many :tool_orders 
+    has_many :tools, through: :tool_orders 
 end
--- app/models/tool_order.rb
+++ app/models/tool_order.rb
@@ -1,2 +1,4 @@
 class ToolOrder < ApplicationRecord
+    belongs_to :tool 
+    belongs_to :order 
 end
