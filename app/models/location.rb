c51bd9f0d4e59b5f46f40f4b5a1e79ebd8bf3fa2
William <willlin1996@gmail.com>
2020-05-05 12:20:02 -0400

    ulptcontrollers

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ApplicationRecord 
+    has_many: orders 
+    has_many :plants, through: :orders 
+    has_many :tools, through: :orders 
+    has_many :users, through: :orders 
 end 
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,2 +1,6 @@
 class Tool < ApplicationRecord
+    has_many :orders
+    has_many :locations, through: :orders 
+    has_many :plants, through: :orders 
+    has_many :users, through: :orders 
 end
