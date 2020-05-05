ed0635c267fd2ecad8d20fcafd734a361f740370
github username <github email address>
2020-05-05 13:10:42 -0400

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+    def index 
+        @users = User.all 
+    end 
+    
     def show 
         @user = User.find(params[:id])
     end
@@ -25,6 +29,7 @@ class UsersController < ApplicationController
     def destroy 
         @user = User.find(params[:id]) 
         @user.destroy 
+        redirect_to users_url 
     end 
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :plant_orders
   resources :orders
   resources :locations
-  resources :users, only: [:new, :create, :show, :delete]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :plants, only: [:index, :show]
   resources :tools, only: [:index, :show]
