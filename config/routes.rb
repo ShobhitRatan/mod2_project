b40274af4659a7dc5de6601219c860fe066499b6
William <willlin1996@gmail.com>
2020-05-05 10:50:47 -0400

    first save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :locations
+  resources :users, only: [:new, :create, :show, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :plants, only: [:index, :show]
+  resources :tools, only: [:index, :show]
 end
