9020681a058f2208d244e379b43d450e4b98b215
github username <github email address>
2020-05-06 18:39:34 -0400

    commit

--- app/controllers/plant_orders_controller.rb
+++ app/controllers/plant_orders_controller.rb
@@ -1,2 +1,44 @@
 class PlantOrdersController < ApplicationController
+    def index 
+        @plant_orders = PlantOrder.all 
+    end 
+
+    def show 
+        @plant_order = PlantOrder.find(params[:id]) 
+    end 
+
+    def new 
+        @plant_order = PlantOrder.new 
+        @plants = Plant.all 
+        @orders = Order.all 
+    end 
+
+    def create 
+        @plant_order = PlantOrder.create(plant_order_params) 
+        redirect_to plant_order_path(@plant_order) 
+    end 
+
+    def edit 
+        @plant_order = PlantOrder.find(params[:id]) 
+        @plants = Plant.all 
+        @orders = Order.all 
+    end 
+
+    def update 
+        @plant_order = PlantOrder.find(params[:id]) 
+        @plant_order.update(plant_order_params) 
+        redirect_to plant_order_path(@plant_order) 
+    end 
+
+    def destroy 
+        @plant_order = PlantOrder.find(params[:id]) 
+        @plant_order.destroy 
+        redirect_to plant_orders_url 
+    end 
+
+    private 
+
+    def plant_order_params 
+        params.require(:plant_order).permit(:plant_id, :order_id, :weight, :line_total) 
+    end 
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,6 @@
 class Location < ApplicationRecord 
     has_many :orders 
-<<<<<<< HEAD
-    has_many :plants, through: :orders 
-    has_many :tools, through: :orders 
-    has_many :users, through: :orders 
-=======
     has_many :plants_orders, through: :orders 
     has_many :tools_orders, through: :orders 
     belongs_to :user
->>>>>>> e740a172adfbfb54184b27d8354c19651a1f89d4
 end 
