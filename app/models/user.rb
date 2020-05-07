df2a849e2816948b03f76b5e1b779ccf26825876
github username <github email address>
2020-05-07 12:38:45 -0400

    Fixed merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
     has_many :orders
     has_many :locations, through: :orders 
-<<<<<<< HEAD
     has_many :plants, through: :orders 
     has_many :tools, through: :orders 
     has_secure_password 
-=======
     has_many :plant_orders, through: :orders 
     has_many :tool_orders, through: :orders 
->>>>>>> master
 end
