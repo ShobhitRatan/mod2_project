e1973ba69f122263fb182624873c3f06fc4048f8
William <willlin1996@gmail.com>
2020-05-07 19:01:24 -0400

    seedingplants, order weight and price

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,13 @@ class Order < ApplicationRecord
     belongs_to :plant 
     belongs_to :tool
 
-    def self.price 
-        self.plant.price + self.tool.price 
+    def price 
+        @order = Order.find(id: self.id)
+        self.price = self.plant.price + self.tool.price
     end 
+
+    def weight 
+        @order = Order.find(id: self.id)
+        self.weight = self.plant.weight + self.tool.weight
+    end
 end
