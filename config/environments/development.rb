205e8bfbb1eddbaf2a7be46443b817a7033ae6e1
ShobhitRatan <shobhit.ratan@baruchmail.cuny.edu>
2021-05-11 02:22:50 +0530

    Adding Action Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,19 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = 
+{
+
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => 'XXXXX@gmail.com',
+  :password           => 'XXXXXXX'
+}
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,19 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.default_url_options = { :host => 'https://greenery-srwl.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = 
+{
+
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => 'XXXXX@gmail.com',
+  :password           => 'XXXXXXX'
+}
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
